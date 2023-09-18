c1bc1a1a115f31eacbadda50e54a33a954b5ba3d
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2023-09-19 05:50:11 +0700

    chore: rename private class property

--- src/Flip/v1/BaseClass.ts
+++ src/Flip/v1/BaseClass.ts
@@ -1,14 +1,14 @@
 import type Flip from '../..';
 
 class BaseV1Class {
-  #flip: typeof Flip;
+  private $flip: typeof Flip;
 
   constructor(flip: typeof Flip) {
-    this.#flip = flip;
+    this.$flip = flip;
   }
 
   protected get baseUrl() {
-    if (this.#flip.toSendBox) {
+    if (this.$flip.toSendBox) {
       return 'kyc-sandbox/api/v1';
     }
 
--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,23 @@ import v1Instance from './axios/v1';
 
 class Flip {
   // eslint-disable-next-line no-use-before-define
-  static #instance: Flip;
-  #secretKey: string | null;
-  #toSendBox: boolean;
-  #v1: FlipV1;
-  #v2: FlipV2;
-  #v3: FlipV3;
+  private static $instance: Flip;
+  $secretKey: string | null;
+  $toSendBox: boolean;
+  $v1: FlipV1;
+  $v2: FlipV2;
+  $v3: FlipV3;
 
   constructor() {
-    this.#toSendBox = true;
-    this.#secretKey = null;
-    this.#v1 = new FlipV1(this);
-    this.#v2 = new FlipV2(this);
-    this.#v3 = new FlipV3(this);
+    this.$toSendBox = true;
+    this.$secretKey = null;
+    this.$v1 = new FlipV1(this);
+    this.$v2 = new FlipV2(this);
+    this.$v3 = new FlipV3(this);
   }
 
   set secretKey(value: string) {
-    this.#secretKey = value;
+    this.$secretKey = value;
 
     const encoded = btoa(`${value}:`);
     instance.defaults.headers.Authorization = `Basic ${encoded}`;
@@ -30,33 +30,33 @@ class Flip {
   }
 
   get secretKey(): string | null {
-    return this.#secretKey;
+    return this.$secretKey;
   }
 
   get toSendBox() {
-    return this.#toSendBox;
+    return this.$toSendBox;
   }
 
   set toSendBox(value: boolean) {
-    this.#toSendBox = value;
+    this.$toSendBox = value;
   }
 
   static get instance() {
-    if (!Flip.#instance) Flip.#instance = new Flip();
+    if (!Flip.$instance) Flip.$instance = new Flip();
 
-    return Flip.#instance;
+    return Flip.$instance;
   }
 
   get v1() {
-    return this.#v1;
+    return this.$v1;
   }
 
   get v2() {
-    return this.#v2;
+    return this.$v2;
   }
 
   get v3() {
-    return this.#v3;
+    return this.$v3;
   }
 }
 
